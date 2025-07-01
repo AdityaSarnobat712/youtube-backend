e85440e306214dab87bc92dc31fa44424d1652b2
AdityaSarnobat712 <adityasarnobat24@gmail.com>
2025-07-01 22:43:19 +0530

    bug fix apierror import line

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,7 +3,6 @@ import {asyncHandler} from "../utils/asyncHandler.js";
 import {User} from "../models/user.model.js"
 import { uploadoncloudinary } from "../utils/cloudinary.js";
 import {ApiResponse} from "../utils/ApiResponse.js"
-import { ApiError } from "../utils/ApiError.js";
 import jwt from "jsonwebtoken"
 import mongoose from "mongoose";
 
